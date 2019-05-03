3137ebabaaa4a4929540b92274b404ba57bdf7b8
j.c.diaconu@student.tudelft.nl <j.c.diaconu@student.tudelft.nl>
2019-05-03 21:26:35 +0200

    Fixed the board

--- src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
+++ src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
@@ -3,16 +3,22 @@ package nl.tudelft.jpacman.npc.ghost;
 import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.level.Level;
 import nl.tudelft.jpacman.level.LevelFactory;
+import nl.tudelft.jpacman.level.PlayerFactory;
 import nl.tudelft.jpacman.points.PointCalculatorLoader;
 import nl.tudelft.jpacman.sprite.PacManSprites;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ClydeTest {
     private static final PacManSprites SPRITES = new PacManSprites();
     private BoardFactory boardFactory;
     private LevelFactory levelFactory;
     private GhostFactory ghostFactory;
+    private PlayerFactory playerFactory;
     private GhostMapParser ghostMapParser;
 
     /**
@@ -21,6 +27,7 @@ public class ClydeTest {
     @BeforeEach void setup() {
         boardFactory = new BoardFactory(SPRITES);
         ghostFactory = new GhostFactory(SPRITES);
+        playerFactory = new PlayerFactory(SPRITES);
         levelFactory = new LevelFactory(SPRITES, ghostFactory, new PointCalculatorLoader().load());
         ghostMapParser = new GhostMapParser(levelFactory, boardFactory, ghostFactory);
     }
@@ -29,8 +36,11 @@ public class ClydeTest {
      *
      */
     @Test void nextAiMoveTestWithin8Squares() {
-        char[][] grid = { ("############").toCharArray(),
-            ("#P        C#").toCharArray(), ("############").toCharArray()};
+        List<String> grid = new ArrayList<>();
+        grid.add(("############"));
+        grid.add(("#P        C#"));
+        grid.add(("############"));
+
         Level level = ghostMapParser.parseMap(grid);
     }
 
