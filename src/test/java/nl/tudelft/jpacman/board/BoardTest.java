759b8584d9fc9e3a14826602c5bbba8b26102967
Nuno Antunes <N.A.CatarinoAntunes@student.tudelft.nl>
2019-05-07 16:48:20 +0200

    Parameterized Tests added to the Board Test in order to test withinBorders method.

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -4,6 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 /**
  * Tests for Board class.
  */
@@ -25,5 +31,74 @@ class BoardTest {
 
         assertThat(board.getHeight()).isEqualTo(1);
         assertThat(board.getWidth()).isEqualTo(1);
+    }
+
+    /**
+     * Generate the arguments for the test function.
+     * Arguments that test the inputs that generate a return that is true.
+     * @return List of arguments
+     */
+    private static Stream<Arguments> withinBordersParametersTrue() {
+        return Stream.of(
+            Arguments.of(0, 0),
+            Arguments.of(0, 1),
+            Arguments.of(1, 0),
+            Arguments.of(1, 1)
+        );
+    }
+
+    /**
+     * Test for values that make withinBorders return true.
+     * @param x x-axis coordinate
+     * @param y y-axis coordinate
+     */
+    @ParameterizedTest
+    @MethodSource("withinBordersParametersTrue")
+    void withinBordersTrueTest(int x, int y) {
+        final int size = 2;
+        Square[][] grid = new BasicSquare[size][size];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                grid[i][j] = new BasicSquare();
+            }
+        }
+
+        Board board = new Board(grid);
+
+        assertThat(board.withinBorders(x, y)).isTrue();
+    }
+
+    /**
+     * Generate the arguments for the test function.
+     * Arguments that test the inputs that generate a return that is false.
+     * @return List of arguments
+     */
+    private static Stream<Arguments> withinBordersParametersFalse() {
+        return Stream.of(
+            Arguments.of(0, 2),
+            Arguments.of(2, 0),
+            Arguments.of(2, 2)
+        );
+    }
+
+    /**
+     * Test for values that make withinBorders return false.
+     * @param x x-axis coordinate
+     * @param y y-axis coordinate
+     */
+    @ParameterizedTest
+    @MethodSource("withinBordersParametersFalse")
+    void withinBordersFalseTest(int x, int y) {
+        final int size = 2;
+        Square[][] grid = new BasicSquare[size][size];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                grid[i][j] = new BasicSquare();
+            }
+        }
+
+        Board board = new Board(grid);
+
+        assertThat(board.withinBorders(x, y)).isFalse();
     }
 }
