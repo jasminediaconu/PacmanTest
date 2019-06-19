f14b88b1466b663d6619fc8488834d0f1caa20f8
Nuno Antunes <N.A.CatarinoAntunes@student.tudelft.nl>
2019-06-19 17:05:22 +0200

    Assignment 3 - Exercise 16

--- src/test/java/nl/tudelft/jpacman/game/MultiLevelGameCasesTest.java
+++ src/test/java/nl/tudelft/jpacman/game/MultiLevelGameCasesTest.java
@@ -1,7 +1,13 @@
 package nl.tudelft.jpacman.game;
 
 import nl.tudelft.jpacman.MultiLevelLauncher;
+import nl.tudelft.jpacman.board.Direction;
+import nl.tudelft.jpacman.level.Level;
+import nl.tudelft.jpacman.level.Player;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MultiLevelGameCasesTest extends GameCasesTest {
 
@@ -13,5 +19,48 @@ public class MultiLevelGameCasesTest extends GameCasesTest {
         setGame(getLauncher().getGame());
         setPlayer(getGame().getPlayers().get(0));
 
+    }
+
+    @Test
+    public void nextLevelAfterWin() {
+        assertThat(getGame().isInProgress()).isFalse();
+
+        getGame().start();
+
+        assertThat(getGame().isInProgress()).isTrue();
+
+        Level initialLevel = getGame().getLevel();
+        Player player = getGame().getPlayers().get(0);
+
+        getGame().move(player, Direction.WEST);
+        getGame().move(player, Direction.WEST);
+
+        assertThat(getGame().getLevel()).isNotEqualTo(initialLevel);
+        assertThat(getGame().isInProgress()).isTrue();
+
+    }
+
+    @Test
+    public void endAfterLastLevelTest() {
+        assertThat(getGame().isInProgress()).isFalse();
+
+        getGame().start();
+
+        assertThat(getGame().isInProgress()).isTrue();
+
+        Player player = getGame().getPlayers().get(0);
+        Level firstLevel = getGame().getLevel();
+
+        getGame().move(player, Direction.WEST);
+        getGame().move(player, Direction.WEST);
+
+        assertThat(getGame().getLevel()).isNotEqualTo(firstLevel);
+
+        Level lastLevel = getGame().getLevel();
+
+        getGame().move(player, Direction.EAST);
+
+        assertThat(getGame().getLevel()).isEqualTo(lastLevel);
+        assertThat(getGame().isInProgress()).isFalse();
     }
 }
